bb64a7e9a7de24fbfc8098b1d655b3fc6fa97517
55lala <lalaimanovaus@gmail.com>
2020-04-10 19:10:31 -0400

    update

--- src/main/java/pages/AbstractPageBase.java
+++ src/main/java/pages/AbstractPageBase.java
@@ -1,4 +1,12 @@
 package pages;
 
+import Utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class AbstractPageBase {
-}
+
+        protected WebDriver driver = Driver.getDriver();
+        protected WebDriverWait wait = new WebDriverWait(driver, 20);
+
+    }
